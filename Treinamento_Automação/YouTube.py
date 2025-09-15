f3a36f3b7483e343cea73a0a69b950e40ade9da5
Jonatas <anjos92489@gmail.com>
2025-09-14 23:23:19 -0300

    Adicionar automacao youtube

---
+++ Treinamento_Automação/YouTube.py
@@ -0,0 +1,11 @@
+import pyautogui as pa
+import time as tm
+
+pa.PAUSE = 2
+pa.press('win')
+pa.typewrite('google')
+pa.press('enter')
+pa.press('tab')
+pa.press('enter')
+pa.typewrite('youtube.com')
+pa.press('enter')
